73cace18087ab500133b312c8759d3589acc619d
Sese <senchyangjabegu@gmail.com>
2026-05-03 22:06:10 +0545

    Initial commit

---
+++ learning/src/App.jsx
@@ -0,0 +1,17 @@
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <h1 className='heading'>Hi I am Sese</h1>
+    <p>Hi I am Prami Gurung</p>
+    </>
+  )
+}
+
+export default App
+
